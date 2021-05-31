c323663961086a8c9172871d17dab91ad52624bf
Teofil-Ioan Vartolomei <teo.vty@gmail.com>
2021-05-31 17:22:33 +0200

    Fix URL bug

--- src/components/RightSideComponent.js
+++ src/components/RightSideComponent.js
@@ -14,7 +14,7 @@ function RightSideComponent(props) {
   const displayData = () => {
     console.log(process.env);
     fetch(
-      `https:api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&appid=${process.env.REACT_APP_API_KEY}`,
+      `https//:api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&appid=${process.env.REACT_APP_API_KEY}`,
     )
       .then((response) => response.json())
       .then((data) => {
